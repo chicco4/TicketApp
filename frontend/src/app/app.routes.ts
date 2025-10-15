fe65e98411715c019e14dca05e760a2ebd196df7
chicco4 <federico.scaggiante@gmail.com>
2025-10-15 16:17:50 +0200

    ok

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'attendee/home',
+    loadComponent: () => import('./pages/attendee/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'organizer/home',
+    loadComponent: () => import('./pages/organizer/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'staff/home',
+    loadComponent: () => import('./pages/staff/home/home').then((m) => m.Home),
+  }
+];
