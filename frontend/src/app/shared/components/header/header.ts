562516aacabce9433facb7c085629361e73a777e
chicco4 <federico.scaggiante@gmail.com>
2025-10-15 16:59:51 +0200

    addeed icons to header

--- frontend/src/app/shared/components/header/header.ts
+++ frontend/src/app/shared/components/header/header.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatButtonModule, RouterLink, RouterLinkActive],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
