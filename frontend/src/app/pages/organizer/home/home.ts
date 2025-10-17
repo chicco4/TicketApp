b71f644ce97401c8a39782cb4f97c4d67157e84a
chicco4 <federico.scaggiante@gmail.com>
2025-10-17 17:45:09 +0200

    org home guide made

--- frontend/src/app/pages/organizer/home/home.ts
+++ frontend/src/app/pages/organizer/home/home.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-organizer-home',
-  imports: [],
+  imports: [MatCardModule, MatIconModule, MatListModule],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrls: ['./home.css']
 })
 export class OrganizerHome {
 
